e02b24c348672a9f0e53e72a6767f4a4cfc2b2a7
frankpagan <frank@cocreate.app>
2023-08-03 17:46:02 -0600

    fix: crud attributes renamed

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -4,8 +4,8 @@ module.exports = {
     "host": "",
     "sources": [
         {
-            "collection": "files",
-            "document": {
+            "array": "files",
+            "object": {
                 "_id": "6025f32c7a78a543a77c9220",
                 "name": "index.html",
                 "path": "/docs/kanban/index.html",
