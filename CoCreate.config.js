3ba66e67ebf80885be0c06b2aa44fff3a3f794d6
frankpagan <frank@cocreate.app>
2023-09-17 19:11:37 -0600

    fix:  Add path and pathname

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -8,17 +8,16 @@ module.exports = {
             "object": {
                 "_id": "6025f32c7a78a543a77c9220",
                 "name": "index.html",
-                "path": "/docs/kanban/index.html",
+                "path": "/docs/kanban",
+                "pathname": "/docs/kanban/index.html",
                 "src": "{{./docs/index.html}}",
                 "host": [
                     "*",
                     "general.cocreate.app"
                 ],
-                "directory": "/docs/kanban",
-                "parentDirectory": "{{parentDirectory}}",
+                "directory": "kanban",
                 "content-type": "{{content-type}}",
-                "public": "true",
-                "website_id": "644d4bff8036fb9d1d1fd69c"
+                "public": "true"
             }
         }
     ]
