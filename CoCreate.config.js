2955e7eea3619e9c7bf525853c93179ca1ffeace
frankpagan <frank@cocreate.app>
2023-11-07 09:47:45 -0600

    fix: update host

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -12,8 +12,7 @@ module.exports = {
                 "pathname": "/docs/kanban/index.html",
                 "src": "{{./docs/index.html}}",
                 "host": [
-                    "*",
-                    "general.cocreate.app"
+                    "*"
                 ],
                 "directory": "kanban",
                 "content-type": "{{content-type}}",
